27b9e00dd1f4519d7c36d7547690de9abeccf5d1
zhaojinghui <zhaojinghui@haodf.com>
2022-08-11 18:12:17 +0800

    loading功能完成

--- src/directive.js
+++ src/directive.js
@@ -1,112 +1,94 @@
 import Vue from 'vue';
 import Loading from './loading.vue';
-import { addClass, removeClass, getStyle, PopupManager, afterLeave } from './util.js';
+import { getStyle, addClass, removeClass } from './util.js';
 const Mask = Vue.extend(Loading);
 
 const loadingDirective = {};
 loadingDirective.install = Vue => {
+  // 是否是服务端渲染，服务端渲染直接return;
   if (Vue.prototype.$isServer) return;
-  const toggleLoading = (el, binding) => {
-    if (binding.value) {
-      Vue.nextTick(() => {
-        if (binding.modifiers.fullscreen) {
-          el.originalPosition = getStyle(document.body, 'position');
-          el.originalOverflow = getStyle(document.body, 'overflow');
-          el.maskStyle.zIndex = PopupManager.nextZIndex();
-
-          addClass(el.mask, 'is-fullscreen');
-          insertDom(document.body, el, binding);
-        } else {
-          removeClass(el.mask, 'is-fullscreen');
-          el.originalPosition = getStyle(el, 'position');
-          insertDom(el, el, binding);
-        }
-      });
-    } else {
-      afterLeave(el.instance, () => {
-        if (!el.instance.hiding) return;
-        el.domVisible = false;
-        const target = binding.modifiers.fullscreen || binding.modifiers.body
-          ? document.body
-          : el;
-        removeClass(target, 'el-loading-parent--relative');
-        removeClass(target, 'el-loading-parent--hidden');
-        el.instance.hiding = false;
-      }, 300, true);
-      el.instance.visible = false;
-      el.instance.hiding = true;
-    }
-  };
-  const insertDom = (parent, el, binding) => {
-    if (!el.domVisible && getStyle(el, 'display') !== 'none' && getStyle(el, 'visibility') !== 'hidden') {
-      Object.keys(el.maskStyle).forEach(property => {
-        el.mask.style[property] = el.maskStyle[property];
-      });
-
-      if (el.originalPosition !== 'absolute' && el.originalPosition !== 'fixed') {
-        addClass(parent, 'el-loading-parent--relative');
-      }
-      if (binding.modifiers.fullscreen && binding.modifiers.lock) {
-        addClass(parent, 'el-loading-parent--hidden');
-      }
-      el.domVisible = true;
-
-      parent.appendChild(el.mask);
-      Vue.nextTick(() => {
-        if (el.instance.hiding) {
-          el.instance.$emit('after-leave');
-        } else {
-          el.instance.visible = true;
-        }
-      });
-      el.domInserted = true;
-    } else if (el.domVisible && el.instance.hiding === true) {
-      el.instance.visible = true;
-      el.instance.hiding = false;
-    }
-  };
-
   Vue.directive('loading', {
     bind: function(el, binding, vnode) {
-      const textExr = el.getAttribute('element-loading-text');
-      const spinnerExr = el.getAttribute('element-loading-spinner');
-      const backgroundExr = el.getAttribute('element-loading-background');
-      const customClassExr = el.getAttribute('element-loading-custom-class');
-      const vm = vnode.context;
+      // 在bind上获取了配置，后续不可修改。
+      const textExr = el.getAttribute('hdf-loading-text');
+      const backgroundExr = el.getAttribute('hdf-loading-background');
+      const customClassExr = el.getAttribute('hdf-loading-custom-class');
+      const imgSrcExr = el.getAttribute('hdf-loading-img-src');
+      const zIndexExr = el.getAttribute('hdf-loading-z-index');
       const mask = new Mask({
         el: document.createElement('div'),
         data: {
-          text: vm && vm[textExr] || textExr,
-          spinner: vm && vm[spinnerExr] || spinnerExr,
-          background: vm && vm[backgroundExr] || backgroundExr,
-          customClass: vm && vm[customClassExr] || customClassExr,
-          fullscreen: !!binding.modifiers.fullscreen
+          text: textExr,
+          background: backgroundExr,
+          customClass: customClassExr,
+          imgSrc: imgSrcExr
         }
       });
+      // el.instance 是el的实例
       el.instance = mask;
+      // mask是具体的dom
       el.mask = mask.$el;
-      el.maskStyle = {};
-
+      // el的样式
+      el.mask.style['zIndex'] = zIndexExr || 2000;
+      // 初始化，如果为true
       binding.value && toggleLoading(el, binding);
     },
 
     update: function(el, binding) {
-      el.instance.setText(el.getAttribute('element-loading-text'));
+      // v-loading的值修改
       if (binding.oldValue !== binding.value) {
         toggleLoading(el, binding);
       }
     },
 
     unbind: function(el, binding) {
+      // 如果已经插入的DOM就执行删除操作
       if (el.domInserted) {
+        // 删除插入的loading的dom;
         el.mask &&
         el.mask.parentNode &&
         el.mask.parentNode.removeChild(el.mask);
+        // 
         toggleLoading(el, { value: false, modifiers: binding.modifiers });
       }
+      // 销毁loading组件实例。
       el.instance && el.instance.$destroy();
     }
   });
+
+  const toggleLoading = (el, binding) => {
+    if (binding.value) {
+      // 当v-loading为true时
+        if (binding.modifiers.fullscreen) {
+          el.mask.addClass();
+          addClass(el.mask, 'is-fullscreen');
+        } else {
+          removeClass(el.mask, 'is-fullscreen');
+        }
+        insertDom(el, binding);
+    } else {
+      removeClass(el, 'hdf-loading-parent-relative');
+      el.instance.visible = false;
+    }
+  };
+  // 1. 被遮盖的dom是不可见的。
+  // 2. 被插入的dom的position问题。(loading.vue中div是absolute)
+  const insertDom = (el, binding) => {
+    el.originalPosition = getStyle(el, 'position');
+    // 如果v-loading的dom已经是不可见的(display = none, visibility = hidden);
+    if (getStyle(el, 'display') !== 'none' && getStyle(el, 'visibility') !== 'hidden') {
+      // el.mask的position是absolute
+      if (el.originalPosition !== 'absolute' && el.originalPosition !== 'fixed' && el.originalPosition !== 'sticky') {
+        addClass(el, 'hdf-loading-parent-relative');
+      }
+      // 插入dom
+      el.appendChild(el.mask);
+      el.domInserted = true;
+      el.instance.visible = true;
+    } else {
+      el.instance.visible = false;
+    }
+  };
 };
 
 export default loadingDirective;
--- src/loading.vue
+++ src/loading.vue
@@ -1,41 +1,29 @@
 <template>
-  <transition name="el-loading-fade" @after-leave="handleAfterLeave">
-    <div
-      v-show="visible"
-      class="el-loading-mask"
-      :style="{ backgroundColor: background || '' }"
-      :class="[customClass, { 'is-fullscreen': fullscreen }]">
-      <div class="el-loading-spinner">
-        <svg v-if="!spinner" class="circular" viewBox="25 25 50 50">
-          <circle class="path" cx="50" cy="50" r="20" fill="none"/>
-        </svg>
-        <i v-else :class="spinner"></i>
-        <p v-if="text" class="el-loading-text">{{ text }}</p>
-      </div>
+  <div
+    v-show="visible"
+    class="hdf-loading-mask"
+    :style="{ backgroundColor: background || '' }"
+    :class="[customClass]">
+    <div class="hdf-loading-spinner">
+      <img v-if="imgSrc" :src="imgSrc"
+      class="hdf-loading-img" alt="loading图片">
+      <p v-if="text" class="hdf-loading-text">{{ text }}</p>
     </div>
-  </transition>
+  </div>
 </template>
-
 <script>
   export default {
     data() {
       return {
+        imgSrc: '',
         text: null,
-        spinner: null,
         background: null,
-        fullscreen: true,
-        visible: false,
-        customClass: ''
+        customClass: '',
+        visible: false
       };
     },
-
-    methods: {
-      handleAfterLeave() {
-        this.$emit('after-leave');
-      },
-      setText(text) {
-        this.text = text;
-      }
+    methods() {
+      
     }
   };
 </script>
